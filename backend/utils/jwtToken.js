76db5f64d763e3c79e321544556e263ab4c332d7
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2024-12-21 22:10:04 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,18 +1,19 @@
+export const sendToken = (user, statusCode, message, res) => {
+  const token = user.getJwtToken();
 
-
-
-export const sendToken = (user, statusCode, message, res) =>{
-
-     const token=  user.getJWTToken();
-     const options = {
-        expires: new Date(Date.now() + 7 * 24 * 60* 60* 1000),
-        httpOnly: true,
-        
-     }
-     res.status(statusCode).cookie("token", token, options).json({
-        success: true,
-        message: "Login successfully",
-        token,
-        user,
-     })
-}
+  // Set the token in the cookie
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Enable for HTTPS
+      sameSite: "strict",
+    })
+    .json({
+      success: true,
+      message,
+      token,
+      user,
+    });
+};
